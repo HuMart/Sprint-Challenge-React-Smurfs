fd645b8553f02f58cae5bd5d628615e2e5fb84ad
Manya0 <manya01@protonmail.com>
2019-05-10 12:13:11 -0400

    App .get data

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,7 +14,14 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => {
+        console.log('res', res)
+      })
+      .catcc(err => {
+        console.log('err', err);
+      })
     this.setState({
       name: '',
       age: '',
